3759c11a5d181068be0bbb930768a303f50183bf
egorSosuPodsolnuh <48887341+egorSosuPodsolnuh@users.noreply.github.com>
2019-03-24 20:37:58 +0300

    Add files via upload

--- task01/complex.h
+++ task01/complex.h
@@ -1,27 +1,58 @@
 #include <iostream>
 #include <iostream>
+#include <math.h>
 
-class Complex
-{
+class Complex {
 public:
-    Complex();
-    Complex(float re, float im = 0.0);
-    Complex(const Complex &copy);
-    ~Complex();
+    Complex()
+            : re(.0),
+              im(.0) {}
 
-    float Re() const;
+    Complex(float a, float b = .0)
+            : re(a),
+              im(b) {}
 
-    float &Re();
+    Complex(const Complex &other) {
+        re = other.re;
+        im = other.im;
+    }
 
-    float Im() const;
+    ~Complex() = default;
 
-    float &Im();
+    float Re() const {
+        return re;
+    }
 
-    float abs();
-    Complex conj();
+    float &Re() {
+        return re;
+    }
 
-    Complex& operator=(const Complex &c);
-    Complex& operator+=(const Complex &c);
+    float Im() const {
+        return im;
+    }
+
+    float &Im() {
+        return im;
+    }
+
+    float abs() {
+        return sqrt(Re() * Re() + Im() * Im());
+    }
+
+    Complex conj() const {
+        return Complex(re, -im);
+    }
+
+    Complex operator=(const Complex &other) {
+        re = other.re;
+        im = other.im;
+    }
+
+    Complex &operator+=(const Complex &c) {
+        re += c.re;
+        im += c.im;
+        return *this;
+    }
 
     // overload this operators:
     // +, -, *(Complex), /(Complex), *(float)
@@ -31,36 +62,82 @@ private:
     float im;
 };
 
-Complex operator+(const Complex &one, const Complex &other);
+float abs(const Complex &z) {
+    return sqrt(z.Re() * z.Re() + z.Im() * z.Im());
+}
 
-Complex operator+(const Complex &one, float other);
+Complex operator+(const Complex &one, const Complex &other) {
+    return Complex(one.Re() + other.Re(), one.Im() + other.Im());
+}
 
-Complex operator+(float other, const Complex &one);
+Complex operator+(const Complex &one, float other) {
+    return Complex(one.Re() + other, one.Im());
+}
 
-Complex operator-(const Complex &one, const Complex &other);
+Complex operator+(float other, const Complex &one) {
+    return Complex(one.Re() + other, one.Im());
+}
 
-Complex operator-(const Complex &one, float other);
+Complex operator-(const Complex &one, const Complex &other) {
+    return Complex(one.Re() - other.Re(), one.Im() - other.Im());
+}
 
-Complex operator-(float other, const Complex &one);
+Complex operator-(const Complex &one, float other) {
+    return Complex(one.Re() - other, one.Im());
+}
 
-Complex operator*(const Complex &one, const Complex &other);
+Complex operator-(float other, const Complex &one) {
+    return Complex(other - one.Re(), -one.Im());
+}
 
-Complex operator*(const Complex &one, float other);
+Complex operator*(const Complex &one, const Complex &other) {
+    return Complex(one.Re() * other.Re() - one.Im() * other.Im(),
+                   one.Re() * other.Im() + one.Im() * other.Re());
+}
 
-Complex operator*(float other, const Complex &one);
+Complex operator*(const Complex &one, float other) {
+    return Complex(one.Re() * other, one.Im() * other);
+}
 
-Complex operator/(const Complex &one, float other);
+Complex operator*(float other, const Complex &one) {
+    return Complex(one.Re() * other, one.Im() * other);
+}
 
-Complex operator/(const Complex &one, const Complex &other);
+Complex operator/(const Complex &one, float other) {
+    return Complex(one.Re() / other, one.Im() / other);
+}
 
-Complex operator/(float other, const Complex &one);
+Complex operator/(const Complex &one, const Complex &other) {
+    float denom_abs = abs(other);
+    Complex
+            inv = other.conj() / (denom_abs * denom_abs);
+    return one * inv;
+}
 
-bool operator==(const Complex &one, const Complex &other);
+Complex operator/(float other, const Complex &one) {
+    float denom_abs = abs(one);
+    Complex
+            inv = one.conj() / (denom_abs * denom_abs);
+    return other * inv;
+}
 
-bool operator!=(const Complex &one, const Complex &other);
+bool operator==(const Complex &one, const Complex &other) {
+    return one.Re() == other.Re() && one.Im() == other.Im();
+}
+
+bool operator!=(const Complex &one, const Complex &other) {
+    return one.Re() != other.Re() || one.Im() != other.Im();
+}
 
 std::ostream &operator<<(std::ostream &out, const Complex &z) {
-    out << z.Re() << " " << z.Im() << "i";
+    out << z.Re();
+    if (z.Im() == 1) {
+        out << " + i";
+    } else if (z.Im() == -1) {
+        out << " - i";
+    } else if (z.Im() != 0) {
+        out << z.Im() << "i";
+    }
     return out;
 }
 
--- task01/main.cpp
+++ task01/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    Complex a(3.0), b(2.0), c(1.0, 1.0);
+    Complex a, b(2.0), c(1.0, 1.0);
 
     std::cin >> a;
     std::cout << a << ", " << b << ", abs: " << b.abs() << std::endl;
