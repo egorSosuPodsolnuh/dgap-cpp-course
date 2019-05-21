9524173fee37da0510ff3eee5c6c2327250aceef
Егор Малхершин <48887341+egorSosuPodsolnuh@users.noreply.github.com>
2019-05-22 00:31:08 +0300

    Update index.cpp

--- task3/index.cpp
+++ task3/index.cpp
@@ -86,3 +86,121 @@ vector<file_info*> read_directory(string path)
 
 
 #endif
+
+
+void FileIndexer::getfiles (string path){
+	vector<file_info*> files= read_directory(path);
+	for (int i=0; i<files.size(); i++){
+	if (files[i]->type==ft_dir){
+		getfiles(files[i]->path);
+	}
+		else{
+			data[files[i]->path]=files[i];
+		}
+	}
+}
+
+unsigned FileIndexer::Build() {
+	getfiles(root_path);
+	return data.size();
+	
+}
+
+void FileIndexer::print_file(std::shared_ptr<file_info> file) {
+	cout << file->name << '\t' << file->path << '\t' << file->size << '\t' << file->mtime << endl;
+	
+}
+
+void FileIndexer::PrintFiles() {
+	for(auto& x: data) {
+		print_file(x.second);
+	}
+	
+	
+}
+
+void FileIndexer::PrintFilesSorted(SortingType type) {
+	switch(type) {
+		case Name:
+			std::vector<std::shared_ptr<file_type>> files;
+			for(auto& x: data) {
+				files.push_back(x.second);	
+			}
+			std::sort(files.begin(), files.end(), [](const std::shared_ptr<file_type> a, const std::shared_ptr<file_type> b) {
+				return a->name < b->name;
+			});
+			for(int i = 0; i < files.size(); ++i)
+				print_file(files[i]);
+			break;
+		case Size:
+			std::vector<std::shared_ptr<file_type>> files;
+			for(auto& x: data) {
+				files.push_back(x.second);	
+			}
+			std::sort(files.begin(), files.end(), [](const std::shared_ptr<file_type> a, const std::shared_ptr<file_type> b) {
+				return a->size < b->size;
+			});
+			for(int i = 0; i < files.size(); ++i)
+				print_file(files[i]);
+			break;
+		case Time:
+			std::vector<std::shared_ptr<file_type>> files;
+			for(auto& x: data) {
+				files.push_back(x.second);	
+			}
+			std::sort(files.begin(), files.end(), [](const std::shared_ptr<file_type> a, const std::shared_ptr<file_type> b) {
+				return a->mtime < b->mtime;
+			});
+			for(int i = 0; i < files.size(); ++i)
+				print_file(files[i]);
+			break;
+	}
+}
+
+unsigned FileIndexer::FindFiles(const std::string &pattern) {
+		auto it = data.find(path);
+	if(it == data.end()) {
+		return 1;
+	} else return 0;
+	
+}
+
+bool FileIndexer::MoveFile(const std::string &path, const std::string &new_path) {
+	auto it = data.find(path);
+	if(it == data.end()) {
+		std::shared_ptr<file_info>& x = data[it.second];
+		data.erase(it);
+		x->path = new_path;
+		data[x->path] = x;
+		return true;
+	} else return false;
+}
+
+bool FileIndexer::DeleteFile(const std::string &path) {
+	auto it = data.find(path);
+	if(it == data.end()) {
+		data.erase(it);
+		return true;
+	} else return false;
+}
+
+
+/*
+enum file_type
+{
+    ft_dir,
+    ft_reg,
+};
+
+struct file_info 
+{
+    string name;
+    string path;
+    file_type type;
+    uint64_t size;
+    uint64_t mtime;
+};
+
+vector<file_info*> read_directory(string path);
+*/
+
